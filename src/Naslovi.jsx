3c068d6cd9c345c4977d005b98b809e0fa09e078
Durdica Stancl <durdicastancl7@gmail.com>
2023-09-07 20:31:28 +0200

    import-export

---
+++ src/Naslovi.jsx
@@ -0,0 +1,23 @@
+//import React from 'react'
+
+export const Naslov = () => {
+  return (
+    <div>Naslov</div>
+  )
+}
+
+
+
+export const Naslov2 = () => {
+    return (
+      <div>Naslov 2</div>
+    )
+  }
+
+  const Glavni = () => {
+    return (
+      <div>Glavni naslov</div>
+    )
+  }
+
+  export default Glavni
